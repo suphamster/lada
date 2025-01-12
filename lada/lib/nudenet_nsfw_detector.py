ff864ce7c1daceefea4013e08254f243a029be03
lada <ladaapp@protonmail.com>
2025-01-12 13:12:10 +0100

    mosaic restoration dataset creation: sample linearly not randomly

--- lada/lib/nudenet_nsfw_detector.py
+++ lada/lib/nudenet_nsfw_detector.py
@@ -5,7 +5,6 @@ from lada.lib.ultralytics_utils import disable_ultralytics_telemetry, convert_yo
 from lada.lib.scene_utils import box_overlap
 from lada.lib import Image, Box
 from ultralytics import YOLO
-import random
 
 disable_ultralytics_telemetry()
 
@@ -18,25 +17,26 @@ class NudeNetNsfwDetector:
     def __init__(self, model: YOLO, device):
         self.model = model
         self.device = device
+        self.batch_size = 4
+        self.min_confidence = 0.2
+        self.min_positive_detections = 4
+        self.sampling_rate = 0.3
 
-    def detect(self, images:list[Image], boxes:Optional[list[Box]]=None, max_samples=20, min_confidence=0.2, min_positive_detections=4, batch_size=4) -> tuple[bool, bool, bool]:
-        if boxes:
-            indices_of_nsfw_elements = list(range(len(images)))
-            indices_of_nsfw_elements = random.sample(indices_of_nsfw_elements, min(len(images), max_samples))
-            samples = itemgetter(*indices_of_nsfw_elements)(images)
-            samples_boxes = itemgetter(*indices_of_nsfw_elements)(boxes)
-        else:
-            samples = random.sample(images, min(len(images), max_samples))
-            samples_boxes = None
+    def detect(self, images:list[Image], boxes:Optional[list[Box]]=None) -> tuple[bool, bool, bool]:
+        num_samples = min(len(images), int(len(images)*self.sampling_rate))
+        indices_step_size = len(images) // num_samples
+        indices_of_nsfw_elements = list(range(0, num_samples*indices_step_size, indices_step_size))
+        samples = itemgetter(*indices_of_nsfw_elements)(images)
+        samples_boxes = itemgetter(*indices_of_nsfw_elements)(boxes) if boxes else None
 
-        batches = [samples[i:i + batch_size] for i in range(0, len(samples), batch_size)]
+        batches = [samples[i:i + self.batch_size] for i in range(0, len(samples), self.batch_size)]
         positive_detections = 0
         positive_male_detections = 0
         positive_female_detections = 0
         for batch_idx, batch in enumerate(batches):
-            batch_prediction_results = self.model.predict(source=batch, stream=False, verbose=False, device=self.device, conf=min_confidence)
+            batch_prediction_results = self.model.predict(source=batch, stream=False, verbose=False, device=self.device, conf=self.min_confidence)
             for result_idx, results in enumerate(batch_prediction_results):
-                sample_idx = batch_idx * batch_size + result_idx
+                sample_idx = batch_idx * self.batch_size + result_idx
                 detections = results.boxes.conf.size(dim=0)
                 if detections == 0:
                     continue
@@ -50,7 +50,7 @@ class NudeNetNsfwDetector:
                 single_image_nsfw_male_detected = False
                 single_image_nsfw_female_detected = False
                 for i in indices_of_nsfw_elements:
-                    if conf[i] > min_confidence and (not samples_boxes or box_overlap(detection_boxes[i], samples_boxes[sample_idx])):
+                    if conf[i] > self.min_confidence and (not samples_boxes or box_overlap(detection_boxes[i], samples_boxes[sample_idx])):
                         if not single_image_nsfw_male_detected:
                             single_image_nsfw_male_detected = cls[i] == MALE_GENITALIA_EXPOSED
                         if not single_image_nsfw_female_detected:
@@ -62,8 +62,8 @@ class NudeNetNsfwDetector:
                     positive_male_detections += 1
                 if single_image_nsfw_female_detected:
                     positive_female_detections += 1
-        nsfw_detected = positive_detections > min_positive_detections
-        nsfw_male_detected = positive_male_detections > min_positive_detections
-        nsfw_female_detected = positive_female_detections > min_positive_detections
+        nsfw_detected = positive_detections > self.min_positive_detections
+        nsfw_male_detected = positive_male_detections > self.min_positive_detections
+        nsfw_female_detected = positive_female_detections > self.min_positive_detections
         #print(f"nudenet nsfw detector: nsfw {nsfw_detected}, detected {positive_detections}/{len(samples)}")
         return nsfw_detected, nsfw_male_detected, nsfw_female_detected
--- lada/lib/watermark_detector.py
+++ lada/lib/watermark_detector.py
@@ -5,7 +5,6 @@ from lada.lib.ultralytics_utils import disable_ultralytics_telemetry, convert_yo
 from lada.lib.scene_utils import box_overlap
 from lada.lib import Image, Box
 from ultralytics import YOLO
-import random
 
 disable_ultralytics_telemetry()
 
@@ -13,32 +12,33 @@ class WatermarkDetector:
     def __init__(self, model: YOLO, device):
         self.model = model
         self.device = device
+        self.batch_size = 4
+        self.min_confidence = 0.2
+        self.min_positive_detections = 4
+        self.sampling_rate = 0.3
 
-    def detect(self, images:list[Image], boxes:Optional[list[Box]]=None, max_samples=20, min_confidence=0.2, min_positive_detections=4, batch_size=4) -> bool:
-        if boxes:
-            indices = list(range(len(images)))
-            indices = random.sample(indices, min(len(images), max_samples))
-            samples = itemgetter(*indices)(images)
-            samples_boxes = itemgetter(*indices)(boxes)
-        else:
-            samples = random.sample(images, min(len(images), max_samples))
-            samples_boxes = None
+    def detect(self, images:list[Image], boxes:Optional[list[Box]]=None) -> bool:
+        num_samples = min(len(images), int(len(images)*self.sampling_rate))
+        indices_step_size = len(images) // num_samples
+        indices = list(range(0, num_samples*indices_step_size, indices_step_size))
+        samples = itemgetter(*indices)(images)
+        samples_boxes = itemgetter(*indices)(boxes) if boxes else None
 
-        batches = [samples[i:i + batch_size] for i in range(0, len(samples), batch_size)]
+        batches = [samples[i:i + self.batch_size] for i in range(0, len(samples), self.batch_size)]
         positive_detections = 0
         for batch_idx, batch in enumerate(batches):
             # not exactly sure why but prediction accuracy is horrible if not setting imgsz to 640 even though model was trained with 512 in train-yolo-watermark-detector.py.
-            batch_prediction_results = self.model.predict(source=batch, stream=False, verbose=False, device=self.device, conf=min_confidence, imgsz=640)
+            batch_prediction_results = self.model.predict(source=batch, stream=False, verbose=False, device=self.device, conf=self.min_confidence, imgsz=640)
             for result_idx, results in enumerate(batch_prediction_results):
-                sample_idx = batch_idx * batch_size + result_idx
+                sample_idx = batch_idx * self.batch_size + result_idx
                 detections = results.boxes.conf.size(dim=0)
                 if detections == 0:
                     continue
                 detection_confidences = results.boxes.conf.tolist()
                 detection_boxes = convert_yolo_boxes(results.boxes, results.orig_shape)
-                single_image_watermark_detected = any(conf > min_confidence and (not samples_boxes or box_overlap(detection_boxes[i], samples_boxes[sample_idx])) for i, conf in enumerate(detection_confidences))
+                single_image_watermark_detected = any(conf > self.min_confidence and (not samples_boxes or box_overlap(detection_boxes[i], samples_boxes[sample_idx])) for i, conf in enumerate(detection_confidences))
                 if single_image_watermark_detected:
                     positive_detections += 1
-        watermark_detected = positive_detections > min_positive_detections
+        watermark_detected = positive_detections > self.min_positive_detections
         #print(f"watermark detector: watermark {watermark_detected}, detected {positive_detections}/{len(samples)}")
         return watermark_detected
