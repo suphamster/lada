8b93fb1b9709e6d0e0aeebb81414f74c7f03efa9
lada <ladaapp@protonmail.com>
2025-01-28 10:41:26 +0100

    upgrade nsfw detection model to v1.3

--- scripts/training/train-nsfw-detection-yolo.py
+++ scripts/training/train-nsfw-detection-yolo.py
@@ -4,5 +4,5 @@ from lada.lib.ultralytics_utils import disable_ultralytics_telemetry, set_defaul
 disable_ultralytics_telemetry()
 set_default_settings()
 
-model = YOLO('yolo11m-seg.yaml')
-model.train(data='configs/yolo/nsfw_detection_dataset_config.yaml', epochs=250, imgsz=640, name="train_nsfw_detection_yolo11m")
+model = YOLO('yolo11m-seg.pt')
+model.train(data='configs/yolo/nsfw_detection_dataset_config.yaml', epochs=200, imgsz=640, name="train_nsfw_detection_yolo11m")
