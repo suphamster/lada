750012abccae060cc026f81eec5a2fa8bd14fd1f
lada <ladaapp@protonmail.com>
2025-03-21 19:15:14 +0100

    refactor: clean-up mosaic dataset creation script

--- lada/lib/nsfw_frame_detector.py
+++ lada/lib/nsfw_frame_detector.py
@@ -1,11 +1,8 @@
-import math
-import random
 from dataclasses import dataclass
 
-import cv2
 import numpy as np
 import ultralytics.models
-from lada.lib import Mask, Box, Image, VideoMetadata
+from lada.lib import Mask, Box, Image
 from lada.lib import mask_utils
 from lada.lib.ultralytics_utils import disable_ultralytics_telemetry, convert_yolo_box, convert_yolo_mask, \
     choose_biggest_detection
@@ -41,52 +38,13 @@ def get_nsfw_frame(yolo_results: ultralytics.engine.results.Results, random_exte
 
     return NsfwFrame(frame_num, yolo_results.orig_img, box, mask)
 
-class NsfwFrameGenerator:
-    def __init__(self, model: ultralytics.models.YOLO, file_path, device=None, random_extend_masks=False, conf=0.25):
+class NsfwImageDetector:
+    def __init__(self, model: ultralytics.models.YOLO, device=None, random_extend_masks=False, conf=0.25):
         self.model = model
-        self.file_path = file_path
         self.device = device
         self.random_extend_masks = random_extend_masks
         self.conf = conf
 
-    def __call__(self, *args, **kwargs) -> NsfwFrame | None:
-        frame = cv2.imread(self.file_path)
-        for results in self.model.predict(source=frame, stream=False, verbose=False, device=self.device, conf=self.conf):
+    def detect(self, file_path: str) -> NsfwFrame | None:
+        for results in self.model.predict(source=file_path, stream=False, verbose=False, device=self.device, conf=self.conf):
             return get_nsfw_frame(results, self.random_extend_masks, 0)
-
-class NsfwFramesGenerator:
-    def __init__(self, model: ultralytics.models.YOLO, video_meta_data: VideoMetadata, device=None, sampling=-1, random_start=False, random_extend_masks=False, conf=0.25):
-        self.sampling = sampling
-        self.model = model
-        self.device = device
-        self.conf = conf
-        self.random_extend_masks = random_extend_masks
-
-        self.video = cv2.VideoCapture(video_meta_data.video_file)
-
-        self.video_fps = video_meta_data.video_fps
-        self.video_length = video_meta_data.frames_count
-
-        self.frame_start = random.randint(0, sampling) if (random_start and sampling > 0) else 0
-        self.frame_end = self.video_length - 1
-
-    def __call__(self, *args, **kwargs):
-        if self.frame_start > self.frame_end or self.frame_start > self.video_length or self.frame_end < 0:
-            return
-
-        frame_num = self.frame_start
-        self.video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-        success, frame = self.video.read()
-        while success and frame_num <= self.frame_end:
-            for results in self.model.predict(source=frame, stream=False, verbose=False, device=self.device, conf=self.conf):
-                nsfw_frame = get_nsfw_frame(results, self.random_extend_masks, frame_num)
-                if nsfw_frame:
-                    yield nsfw_frame
-
-                if self.sampling != -1:
-                    frame_num += math.ceil(self.sampling * self.video_fps)
-                    self.video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-                else:
-                    frame_num += 1
-
-                success, frame = self.video.read()
--- scripts/dataset_creation/create-mosaic-detection-dataset.py
+++ scripts/dataset_creation/create-mosaic-detection-dataset.py
@@ -1,19 +1,19 @@
 import argparse
 import os
 import random
-from concurrent import futures
-from os import path as osp
 from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
+from os import path as osp
 from pathlib import Path
 
 import cv2
 import numpy as np
 from ultralytics import YOLO
 
-from lada.lib.nsfw_frame_detector import NsfwFramesGenerator, NsfwFrameGenerator, NsfwFrame
-from lada.lib.ultralytics_utils import disable_ultralytics_telemetry
+from lada.lib import visualization_utils, mask_utils, degradation_utils, image_utils
 from lada.lib.mosaic_utils import addmosaic_base, get_random_parameter
-from lada.lib import visualization_utils, video_utils, mask_utils, degradation_utils, image_utils
+from lada.lib.nsfw_frame_detector import NsfwImageDetector
+from lada.lib.threading_utils import clean_up_completed_futures
+from lada.lib.ultralytics_utils import disable_ultralytics_telemetry
 
 disable_ultralytics_telemetry()
 
@@ -22,7 +22,11 @@ def crop_to_box(img, box):
     cropped_img = img[t:b + 1, l:r + 1]
     return cropped_img
 
-def process_frame(nsfw_frame: NsfwFrame, file_path, output_root, show=False, window_name="mosaic"):
+def process_image_file(file_path, nsfw_frame_generator, output_root, show=False, window_name="mosaic"):
+    nsfw_frame = nsfw_frame_generator()
+    if not nsfw_frame:
+        return
+
     img = nsfw_frame.frame
     mask = nsfw_frame.mask
     box = nsfw_frame.box
@@ -76,20 +80,6 @@ def process_frame(nsfw_frame: NsfwFrame, file_path, output_root, show=False, win
                     [int(cv2.IMWRITE_JPEG_QUALITY), 95])
         cv2.imwrite(f"{output_root}/mask/{name}-{nsfw_frame.frame_number:06d}.png", mask_mosaic)
 
-def process_video_file(file_path, output_root, model, device, sampling=60, show=False, window_name="mosaic"):
-    video_meta_data = video_utils.get_video_meta_data(file_path)
-    nsfw_frame_generator = NsfwFramesGenerator(model, video_meta_data, device, sampling, random_extend_masks=True, random_start=True, conf=0.75)
-
-    nsfw_frame: NsfwFrame
-    for nsfw_frame in nsfw_frame_generator():
-        process_frame(nsfw_frame, file_path, output_root, show=show, window_name=window_name)
-
-def process_image_file(file_path, output_root, model, device, show=False, window_name="mosaic"):
-    nsfw_frame_generator = NsfwFrameGenerator(model, file_path, device, random_extend_masks=True, conf=0.75)
-    nsfw_frame = nsfw_frame_generator()
-    if nsfw_frame:
-        process_frame(nsfw_frame, file_path, output_root, show=show, window_name=window_name)
-
 def get_files(dir, filter_func):
     file_list = []
     for r, d, f in os.walk(dir):
@@ -99,26 +89,15 @@ def get_files(dir, filter_func):
                 file_list.append(Path(file_path))
     return file_list
 
-def clean_up_completed_futures(completed_futures):
-    for job in futures.as_completed(completed_futures):
-        exception = job.exception()
-        if exception:
-            print(f"ERR(main): failed processing file: {type(exception).__name__}: {exception}")
-            # raise exception # todo: for some images degradation_util::_apply_video_compression throws an exception. lets ignore and skip those for now...
-        completed_futures.remove(job)
-
 def parse_args():
     parser = argparse.ArgumentParser("Create mosaic detection dataset")
     parser.add_argument('--output-root', type=Path, help="directory where resulting images/masks are saved")
     parser.add_argument('--input-root', type=Path, help="directory containing video files")
     parser.add_argument('--device', type=str, default="cuda:0")
-    parser.add_argument('--model', type=str, default="model_weights/lada_nsfw_detection_model.pt", help="path to YOLO model")
+    parser.add_argument('--model', type=str, default="model_weights/lada_nsfw_detection_model_v1.3.pt", help="path to YOLO model")
     parser.add_argument('--workers', type=int, default=4, help="number of worker threads")
     parser.add_argument('--start-index', type=int, default=0, help="Can be used to continue a previous run. Note the index number next to last processed file name")
-    parser.add_argument('--sampling', type=int, default=60, help="process only 1 frame every <sampling> seconds instead of every frame in the video")
     parser.add_argument('--show', default=False, action=argparse.BooleanOptionalAction, help="show each sample")
-    parser.add_argument('--videos', default=True, action=argparse.BooleanOptionalAction)
-    parser.add_argument('--images', default=False, action=argparse.BooleanOptionalAction)
     parser.add_argument('--max-file-limit', type=int, default=None, help="instead of processing all files found in input-root dir it will choose files randomly up to the given limit")
 
     args = parser.parse_args()
@@ -128,27 +107,16 @@ def main():
     args = parse_args()
 
     model = YOLO(args.model)
+    nsfw_frame_generator = NsfwImageDetector(model, args.device, random_extend_masks=True, conf=0.75)
 
-    if args.show:
-        os.environ["QT_QPA_PLATFORM"] = "xcb"
-        window_name = 'mosaic'
-        cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
-    else:
+    if not args.show:
         os.makedirs(f"{args.output_root}/mask", exist_ok=True)
         os.makedirs(f"{args.output_root}/img", exist_ok=True)
         jobs = []
 
-    selected_files = []
-    if args.videos:
-        video_files = get_files(args.input_root, video_utils.is_video_file)
-        if args.max_file_limit and len(video_files) > args.max_file_limit:
-            video_files = random.choices(video_files, k=args.max_file_limit)
-        selected_files += video_files
-    if args.images:
-        image_files = get_files(args.input_root, image_utils.is_image_file)
-        if args.max_file_limit and len(image_files) > args.max_file_limit:
-            image_files = random.choices(image_files, k=args.max_file_limit)
-        selected_files += image_files
+    selected_files = get_files(args.input_root, image_utils.is_image_file)
+    if args.max_file_limit and len(selected_files) > args.max_file_limit:
+        selected_files = random.choices(selected_files, k=args.max_file_limit)
 
     with ThreadPoolExecutor(max_workers=args.workers) as executor:
         for file_idx, file_path in enumerate(selected_files):
@@ -156,14 +124,10 @@ def main():
                 print(f"{file_idx}, Skipping {file_path.name}: Already processed")
                 continue
             print(f"{file_idx}, Processing {file_path.name}")
-            is_video_file = video_utils.is_video_file(file_path)
             if args.show:
-                process_video_file(file_path, args.input_root, args.output_root, args.show) if is_video_file else process_image_file(file_path, args.output_root, model, args.device, args.show)
+                process_image_file(file_path, args.output_root, nsfw_frame_generator, args.show)
             else:
-                if is_video_file:
-                    jobs.append(executor.submit(process_video_file, file_path, args.output_root, model, args.device, args.sampling, args.show))
-                else:
-                    jobs.append(executor.submit(process_image_file, file_path, args.output_root, model, args.device, args.show))
+                jobs.append(executor.submit(process_image_file, file_path, args.output_root, nsfw_frame_generator))
                 clean_up_completed_futures(jobs)
     wait(jobs, return_when=ALL_COMPLETED)
     clean_up_completed_futures(jobs)
