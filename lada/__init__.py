3e99e5e1744f49e199f6e9d7dddd9e58b4de3159
lada <ladaapp@protonmail.com>
2025-03-24 18:18:57 +0100

    Don't leak flatpak packaging information into lib

--- lada/__init__.py
+++ lada/__init__.py
@@ -1,12 +1,12 @@
 import os
 
-MODEL_WEIGHTS_DIR = '/app/model_weights' if "FLATPAK_ID" in os.environ else "model_weights"
-
-if "FLATPAK_ID" in os.environ:
-  os.environ["YOLO_CONFIG_DIR"] = "/var/config/yolo"
+if "LADA_MODEL_WEIGHTS_DIR" in os.environ:
+  MODEL_WEIGHTS_DIR = os.environ["LADA_MODEL_WEIGHTS_DIR"]
+else:
+  MODEL_WEIGHTS_DIR = "model_weights"
 
 os.environ["NO_ALBUMENTATIONS_UPDATE"] = "1"
 
 VERSION = '0.6.0-dev'
 
-LOG_LEVEL = os.environ["LOG_LEVEL"] if "LOG_LEVEL" in os.environ else 'WARNING'
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "WARNING")
